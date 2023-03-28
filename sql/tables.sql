5deab13fe9ca0ec5cc530722924d77c7204428cf
ppaliani <ppaliani@emich.edu>
2023-03-27 22:24:40 -0400

    Finished create table statements

--- sql/tables.sql
+++ sql/tables.sql
@@ -21,20 +21,23 @@
 -- );
 
 
-CREATE TABLE books (
-    ISBN VARCHAR(255) NOT NULL PRIMARY KEY,
+CREATE TABLE book (
+    ISBN VARCHAR(13) NOT NULL PRIMARY KEY,
     author VARCHAR(255) NOT NULL,
     title VARCHAR(255) NOT NULL,
     publisher VARCHAR(255) NOT NULL,
     date_published DATE NOT NULL,
     genre VARCHAR(255) NOT NULL,
-    price FLOAT NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    price INTEGER NOT NULL,
+    description VARCHAR(255),
     num_of_copies INTEGER NOT NULL
 );
 
-CREATE TABLE reviews (
-
+CREATE TABLE review (
+    username VARCHAR(255),
+    ISBN VARCHAR(13),
+    FOREIGN KEY (ISBN) REFERENCES book(ISBN)
+    FOREIGN KEY (username) REFERENCES user(username)
 );
 
 CREATE TABLE user (
@@ -46,10 +49,8 @@ CREATE TABLE user (
     city VARCHAR(255) NOT NULL,
     state CHAR(2) NOT NULL,
     zip INTEGER NOT NULL,
-    cardNo VARCHAR(16) NOT NULL,
-
-    FOREIGN KEY (cardNo) REFERENCES credit(cardNo)
-    FOREIGN KEY 
+    card_no VARCHAR(16) NOT NULL,
+    FOREIGN KEY (card_no) REFERENCES credit(card_no)
 );
 
 CREATE TABLE purchase (
@@ -59,36 +60,31 @@ CREATE TABLE purchase (
     tax FLOAT NOT NULL,
     shipping_cost FLOAT NOT NULL,
     total FLOAT NOT NULL
-    ISBN VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    FOREIGN KEY (ISBN) REFERENCES books(ISBN),
     FOREIGN KEY (username) REFERENCES user(username),
 );
 
 CREATE TABLE credit (
-    cardNo VARCHAR(16) NOT NULL PRIMARY KEY,
-    secCode INTEGER NOT NULL,
-    expDate DATE NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    card_no VARCHAR(16) NOT NULL PRIMARY KEY,
+    sec_code INTEGER NOT NULL,
+    exp_date DATE NOT NULL,
     username VARCHAR(255) NOT NULL,
-    PIN VARCHAR(255) NOT NULL,
     FOREIGN KEY (username) REFERENCES user(username),
-    FOREIGN KEY (PIN) REFERENCES user(PIN)
 );
 
 CREATE TABLE cart (
     quantity INTEGER NOT NULL,
     username VARCHAR(255) NOT NULL,
-    ISBN INTEGER NOT NULL,
+    ISBN VARCHAR(13) NOT NULL,
     PRIMARY KEY (username, ISBN)
     FOREIGN KEY (ISBN) REFERENCES book(ISBN),
     FOREIGN KEY (username) REFERENCES user(username)
 );
 
-CREATE TABLE past_orders (
+CREATE TABLE past_cart (
     quantity INTEGER NOT NULL,
     purchase_id INTEGER NOT NULL,
-    ISBN INTEGER NOT NULL,
+    ISBN VARCHAR(13) NOT NULL,
     PRIMARY KEY (purchase_id, ISBN)
     FOREIGN KEY (ISBN) REFERENCES book(ISBN),
     FOREIGN KEY (purchase_id) REFERENCES purchase(purchase_id)
