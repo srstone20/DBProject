04aace9800b8c861d8ddad7572cab7f0d0ed1c39
Sam S <sstone20@emich.edu>
2023-04-14 22:18:08 -0400

    ??

--- static/js/userInfo.js
+++ static/js/userInfo.js
@@ -1,14 +1,15 @@
-// function getUserInfo() {
-//     if (u == null || u == "") {
-//         return;
-//     }
-//     var tags = document.getElementsByClassName("disp-username");
-//     for (var i = 0; i < tags.length; i++) {
-//         tags[i].innerHTML = sessionStorage.getItem("username");
-//         tags[i].value = sessionStorage.getItem("username");
-//     }
-// }
-
 function getUserInfo() {
-    console.log("GETTING IT");
+    var u = sessionStorage.getItem("username");
+    if (u == null || u == "") {
+        return;
+    }
+    var tags = document.getElementsByClassName("disp-username");
+    for (var i = 0; i < tags.length; i++) {
+        tags[i].innerHTML = u;
+        tags[i].value = u;
+    }
 }
+
+// function getUserInfo() {
+//     console.log("GETTING IT");
+// }
