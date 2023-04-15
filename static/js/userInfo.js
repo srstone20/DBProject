91e43c4dcfb3daee9e2386fb4cf79a1f5b702958
Sam S <sstone20@emich.edu>
2023-04-14 22:13:19 -0400

    I'm going crazy

--- static/js/userInfo.js
+++ static/js/userInfo.js
@@ -1,10 +1,14 @@
+// function getUserInfo() {
+//     if (u == null || u == "") {
+//         return;
+//     }
+//     var tags = document.getElementsByClassName("disp-username");
+//     for (var i = 0; i < tags.length; i++) {
+//         tags[i].innerHTML = sessionStorage.getItem("username");
+//         tags[i].value = sessionStorage.getItem("username");
+//     }
+// }
+
 function getUserInfo() {
-    if (u == null || u == "") {
-        return;
-    }
-    var tags = document.getElementsByClassName("disp-username");
-    for (var i = 0; i < tags.length; i++) {
-        tags[i].innerHTML = sessionStorage.getItem("username");
-        tags[i].value = sessionStorage.getItem("username");
-    }
+    console.log("GETTING IT");
 }
