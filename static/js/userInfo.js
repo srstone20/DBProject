c76edbf38cfab45f14deb37abab90cee47786cea
Sam S <sstone20@emich.edu>
2023-04-14 21:48:16 -0400

    Add file back

---
+++ static/js/userInfo.js
@@ -0,0 +1,10 @@
+function getUserInfo() {
+    if (u == null || u == "") {
+        return;
+    }
+    var tags = document.getElementsByClassName("disp-username");
+    for (var i = 0; i < tags.length; i++) {
+        tags[i].innerHTML = sessionStorage.getItem("username");
+        tags[i].value = sessionStorage.getItem("username");
+    }
+}
